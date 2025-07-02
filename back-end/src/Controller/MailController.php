2ad5cd2a8f84a59ecd7fca61470ed201665d4084
Priyank Solanki <psolanki@condorcet93.fr>
2025-07-02 21:37:01 +0200

    user controller

--- back-end/src/Controller/MailController.php
+++ back-end/src/Controller/MailController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Api;
+namespace App\Controller;
 
 use App\Entity\Mail;
 use App\Entity\User;
---
+++ back-end/src/Controller/UserController.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\User;
+use App\Repository\UserRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/api/users', name: 'api_users_')]
+class UserController extends AbstractController
+{
+
+    #[Route('/{id}', methods: ['GET'])]
+    public function show(User $user): JsonResponse
+    {
+        return $this->json([
+            'id' => $user->getId(),
+            'email' => $user->getEmail(),
+            'displayName' => $user->getDisplayName(),
+            'microsoftId' => $user->getMicrosoftId(),
+        ]);
+    }
+
+    #[Route('{microsoftId}', methods: ['POST'])]
+    public function create(Request $request, EntityManagerInterface $em): JsonResponse
+    {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['email'], $data['displayName'], $data['microsoftId'])) {
+            return $this->json(['error' => 'Missing required fields'], Response::HTTP_BAD_REQUEST);
+        }
+
+        if($em->getRepository(User::class)->findOneBy(['microsoftId' => $data['microsoftId']])){
+            return $this->json(['error' => 'User already exists'], Response::HTTP_BAD_REQUEST);
+        }
+
+        $user = new User();
+        $user->setEmail($data['email']);
+        $user->setDisplayName($data['displayName']);
+        $user->setMicrosoftId($data['microsoftId']);
+
+        $em->persist($user);
+        $em->flush();
+
+        return $this->json(['id' => $user->getId()], Response::HTTP_CREATED);
+    }
+
+    #[Route('/{id}', methods: ['DELETE'])]
+    public function delete(User $user, EntityManagerInterface $em): JsonResponse
+    {
+        $em->remove($user);
+        $em->flush();
+
+        return $this->json(null, Response::HTTP_NO_CONTENT);
+    }
+}
